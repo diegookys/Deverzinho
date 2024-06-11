465b0f35519a3bddca0bec662a674c5d0eda047c
Diego Santos <133829130+diegookys@users.noreply.github.com>
2024-06-11 18:00:21 -0300

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,12 @@
+// Classe mãe
+
+public class Person{
+    protected String nome;
+    protected int idade;
+
+    // Construtor
+    public Person(String nome, int idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
