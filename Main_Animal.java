85ff4d9436eee12fdd859d1170a909e55b3df2a4
Diego Santos <133829130+diegookys@users.noreply.github.com>
2024-06-11 18:01:39 -0300

    Create Main_Animal.java

---
+++ Main_Animal.java
@@ -0,0 +1,9 @@
+public class Main_Animal {
+    public static void main(String[] args) {
+        Animal umdog = new Dog();
+        Animal umcat = new Cat();
+
+        umdog.makeSound();
+        umcat.makeSound();
+    }
+}
